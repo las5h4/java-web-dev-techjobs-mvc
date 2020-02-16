7283edec31cbde9c401583788c65aefc4081a055
las5h4 <loganasmalley@gmail.com>
2020-02-16 15:48:36 -0600

    moved columnChoices to TechJobsController, all bonus missions complete

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/ListController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/ListController.java
@@ -18,15 +18,11 @@ import java.util.HashMap;
 @RequestMapping(value = "list")
 public class ListController extends TechJobsController {
 
-    static HashMap<String, String> columnChoices = new HashMap<>();
+
     static HashMap<String, Object> tableChoices = new HashMap<>();
 
     public ListController () {
-        columnChoices.put("all", "All");
-        columnChoices.put("employer", "Employer");
-        columnChoices.put("location", "Location");
-        columnChoices.put("positionType", "Position Type");
-        columnChoices.put("coreCompetency", "Skill");
+
 
         ArrayList<String> all = new ArrayList<>();
         all.add("View All");
@@ -40,7 +36,7 @@ public class ListController extends TechJobsController {
 
     @RequestMapping(value = "")
     public String list(Model model) {
-        model.addAttribute("columns", columnChoices);
+//        model.addAttribute("columns", columnChoices);
         model.addAttribute("tableChoices", tableChoices);
         model.addAttribute("employers", JobData.getAllEmployers());
         model.addAttribute("locations", JobData.getAllLocations());
